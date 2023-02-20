9a50437c848e84b103f80b0f4cdf23e2211d31c7
Oficky <owolabi_taofeek19@yahoo.com>
2023-02-20 08:36:37 -0800

    Task 5.  program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		putchar(m + '0');
+	}
+	putchar('\n');
+	return (0);
+}
